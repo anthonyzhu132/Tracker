0168d03ee329453d700d9fccfa3be50c6e085efa
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-26 22:44:16 -0700

    added config npm to configure API keys

--- tracker-server/src/index.js
+++ tracker-server/src/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
-
+const { MongoURI } = require('./config');
 const app = express();
+const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://anthonyzhu132:<password>@cluster0.m29e0.mongodb.net/<dbname>?retryWrites=true&w=majority'
+// mongoose.connect(URI, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true
+// });
+
+// mongoose.connection.on('connected', () => {
+//   console.log('Connected to MongoDB')
+// });
+
+// mongoose.connection.on('error', (err) => {
+//   console.error('Error connecting to MongoDB', err)
+// });
 
 app.get('/', (req, res) => {
   res.send('Hi there from root')
 });
 
 app.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('listening on port 3000' + MongoURI);
 });
