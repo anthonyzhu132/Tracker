6ff1df51e75355f5466b3e3e21374194fa2b9998
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-10-26 22:32:30 -0700

    added information for mongoDB

--- tracker-server/src/index.js
+++ tracker-server/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const URI = 'mongodb+srv://anthonyzhu132:<password>@cluster0.m29e0.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
 app.get('/', (req, res) => {
   res.send('Hi there from root')
 });
