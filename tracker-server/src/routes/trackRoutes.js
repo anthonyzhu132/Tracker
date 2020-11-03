cb16507af8dc2a2566c44f104fd1e78f71f9a44e
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-02 19:02:56 -0800

    added checks for tracks criteria

--- tracker-server/src/routes/trackRoutes.js
+++ tracker-server/src/routes/trackRoutes.js
@@ -21,11 +21,14 @@ router.post('/tracks', async (req, res) => {
     return res.status(422).send({ error: "Must provide name and location" });
   }
 
-  const track = new Track({ name, locations, userId: req.user._id });
-
-  await track.save();
-
-  res.send(track);
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
+    res.send(track);
+  } catch(err) {
+    res.status(422).send({ error: err.message });
+  }
+  
 });
 
 module.exports = router;
