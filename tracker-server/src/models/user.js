6b0fcabd5cd715452734c4d853c5e6f9cf704aa7
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-02 17:49:59 -0800

    added a check to see if users entered password is password that is stored

--- tracker-server/src/models/user.js
+++ tracker-server/src/models/user.js
@@ -13,26 +13,51 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+//What to do before saving users
 userSchema.pre('save', function(next) {
   const user = this;
 
+  //check if user password is modified
   if(!user.isModified('password')) {
     return next();
   }
 
+  //generate salt 
   bcrypt.genSalt(10, (err, salt) => {
     if(err) {
       return next(err);
     }
 
+    //Hash user password with salt
     bcrypt.hash(user.password, salt, (err, hash) => {
       if(err) {
         return next(err);
       }
+      
+      //set user password with new hashed password
       user.password = hash;
       next();
     })
   });
 });
+
+userSchema.methods.comparePassword = function(enteredPassword) {
+  const user = this;
+
+  //Making a new promise to resolve entered password and stored password
+  return new Promise ((resolve, reject) => {
+    bcrypt.compare(enteredPassword, user.password, (err, isMatch) => {
+      if(err) {
+        return reject(err);
+      }
+
+      if(!isMatch) {
+        return reject(false);
+      }
+
+      resolve(true);
+    });
+  })
+}
 
 mongoose.model('User', userSchema);
