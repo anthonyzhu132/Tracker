bf2551336d6080ddce32563c8fb070021f71316c
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-02 18:23:34 -0800

    added sign in route

--- tracker-server/src/routes/authRoutes.js
+++ tracker-server/src/routes/authRoutes.js
@@ -18,4 +18,29 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  if(!email || !password) {
+    return res.status(422).send({ error: "Must provide email & password" });
+  };
+
+  const user = await User.findOne({ email });
+
+  if(!user) {
+    return res.status(422).send({ error: "Invalid password or email"});
+  };
+
+  try{
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'SECRET_KEY');
+
+    res.send({ token });
+
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password or email" });
+  };
+
+});
+
 module.exports = router; 
