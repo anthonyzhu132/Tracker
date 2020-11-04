9d043f04097f3f24da30eb821249e44c446eb714
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-03 21:04:32 -0800

    added tracks file back with boilerplateto fix git issue

---
+++ tracks/App.js
@@ -0,0 +1,6 @@
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
