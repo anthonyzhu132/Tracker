ee357bd547432dae0da8971aca772237502402d9
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-03 21:27:20 -0800

    added navigation for TracklistShow

--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const TrackListScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+const TrackListScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+      <Button title="Track Detail" onPress={() => navigation.navigate('TrackDetail')} />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({});
