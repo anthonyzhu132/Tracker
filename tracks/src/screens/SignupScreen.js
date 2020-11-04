9dfa4c8f0fd337793c62ebf3575de8f566edc704
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-03 21:40:52 -0800

    created spacer component

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Button, Text, Input } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
   return (
     <View>
-      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-      <Button title="Sign in" onPress={() => navigation.navigate('Signin')} />
-      <Button title="Main flow" onPress={() => navigation.navigate('mainFlow')} />
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
     </View>
   );
 };
