59cbad593e6fe126ce3fa071f4e5c6c8a0ba97c8
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-03 21:24:51 -0800

    added navigation for SignupScreen

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-const SignupScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+const SignupScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
+      <Button title="Sign in" onPress={() => navigation.navigate('Signin')} />
+      <Button title="Main flow" onPress={() => navigation.navigate('mainFlow')} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
