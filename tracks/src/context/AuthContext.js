c9dc8cb3b9dbab4ddee046e08a234db259369f21
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-04 17:50:44 -0800

    added error handling and shows user error if user is already signed up

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,15 +1,24 @@
 import createDataContext from './createDataContext';
+import trackerAPI from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   };
 };
 
 const signup = (dispatch) => {
-  return ({ email, password }) => {
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerAPI.post('/signup', {email, password });
 
+      console.log(response.data);
+    } catch (err) {
+      dispatch({ type: 'add_error', payload: 'Something went wrong at sign up' })
+    }
   };
 };
 
@@ -26,4 +35,4 @@ const signout = (dispatch) => {
 };
 
 
-export const { Provider, Context } = createDataContext(authReducer, { signin, signout, signup }, { isSignedIn: false });
+export const { Provider, Context } = createDataContext(authReducer, { signin, signout, signup }, { isSignedIn: false, errorMessage: '' });
--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Button, Text, Input } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -29,8 +31,9 @@ const SignupScreen = ({ navigation }) => {
       autoCorrect={false}
       secureTextEntry
       />
+      {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
       <Spacer>
-        <Button title="Sign Up" />
+        <Button title="Sign Up" onPress={() => signup({ email, password })}/>
       </Spacer>
     </View>
   );
@@ -47,6 +50,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 250
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: 'red'
   }
 });
 
