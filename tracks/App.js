be8c530eeec398cd7221b4dec784c4af0ba4d086
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-04 17:15:53 -0800

    wrapped entire createAppContainer with AuthProvider Context

--- tracks/App.js
+++ tracks/App.js
@@ -9,6 +9,8 @@ import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
 
+import { Provider as AuthProvider } from './src/context/AuthContext';
+
 const switchNavigator = createSwitchNavigator({
   loginFlow: createStackNavigator({
     Signup: SignupScreen,
@@ -24,4 +26,12 @@ const switchNavigator = createSwitchNavigator({
   })
 });
 
-export default createAppContainer(switchNavigator);
+const App =  createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  )
+}
