a7ca40f158fbbf3656d2fa1e416afc024d302c53
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-16 16:47:10 -0800

    removed random location generator for polyline

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -3,14 +3,6 @@ import { StyleSheet } from 'react-native';
 import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
-  let points = [];
-
-  for (let i = 0; i< 20; i++) {
-    points.push({
-      latitude: 49.229190 + i * 0.001,
-      longitude: -123.064830 + i * 0.001
-    })
-  }
   
   return <>
     <MapView
@@ -22,7 +14,7 @@ const Map = () => {
       longitudeDelta: 0.01
     }}
     >
-      <Polyline coordinates={points} />
+      
     </MapView>
   </>
 };
--- tracks/src/context/LocationContext.js
+++ tracks/src/context/LocationContext.js
@@ -2,6 +2,8 @@ import createDataContext from './createDataContext';
 
 const locationReducer = (state, action) => {
   switch (action.type) {
+    case 'add_current_location':
+      return { ...state, currentLocation: action.payload }
     default:
       return state;
   };
@@ -20,8 +22,8 @@ const stopRecording = (dispatch) => {
 };
 
 const addLocation = (dispatch) => {
-  return () => {
-
+  return (location) => {
+    dispatch({ type: 'add_current_location', payload: location })
   };
 };
 
