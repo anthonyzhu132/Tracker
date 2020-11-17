5665c6981c15ad8de5563e39d158f27e218e2097
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-16 17:01:10 -0800

    added stylign for circle

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { ActivityIndicator, StyleSheet } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 
@@ -13,19 +13,26 @@ const Map = () => {
 
   return (
     <MapView
-    style={styles.map} 
-    initialRegion={{
-      ...currentLocation.coords,
-      latitudeDelta: 0.01,
-      longitudeDelta: 0.01
-    }}
-    region={{
-      ...currentLocation.coords,
-      latitudeDelta: 0.01,
-      longitudeDelta: 0.01
-    }}
+      style={styles.map} 
+      initialRegion={{
+        ...currentLocation.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+      region={{
+        ...currentLocation.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
     >
-      
+
+      <Circle
+        center={currentLocation.coords}
+        radius={120}
+        strokeColor="rgba(158, 158, 255, 1.0)"
+        fillColor="rgba(158, 158, 255, 0.3)"
+      />
+
     </MapView>
   );
 };
