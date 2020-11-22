bb7ea0dc5639addbf8413af1f49bf2ebc5b6b452
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-21 17:54:02 -0800

    created save track function from useSaveTrack.js hook

--- tracks/src/hooks/useSaveTrack.js
+++ tracks/src/hooks/useSaveTrack.js
@@ -3,5 +3,16 @@ import { Context as TrackContext } from '../context/TrackContext';
 import { Context as LocationContext } from '../context/LocationContext';
 
 export default () => {
+  //Pulling out createTrack action
+  const { createTrack } = useContext(TrackContext);
 
+  //Pulling out Location and name from state object from LocationContext
+  const { state: { locations, name }} = useContext(LocationContext);
+
+  //Putting createTrack in a variable to call anytime 
+  const saveTrack = () => {
+    createTrack(name, locations);
+  };
+
+  return [saveTrack];
 };
