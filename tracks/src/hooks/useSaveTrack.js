757e2e2ce0d2b68ed978c947a2a9ea5425f8333b
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-21 17:51:04 -0800

    added boilerplate for useSaveTrack hook

---
+++ tracks/src/hooks/useSaveTrack.js
@@ -0,0 +1,7 @@
+import { useContext } from 'react';
+import { Context as TrackContext } from '../context/TrackContext';
+import { Context as LocationContext } from '../context/LocationContext';
+
+export default () => {
+
+};
