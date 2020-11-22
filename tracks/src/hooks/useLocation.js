93807524581dad867e178ecff305f3e16744ae9a
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-21 17:19:30 -0800

    added startWtaching function info useEWffect

--- tracks/src/hooks/useLocation.js
+++ tracks/src/hooks/useLocation.js
@@ -5,27 +5,27 @@ export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
   const [subscriber, setSubscriber] = useState(null);
 
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-      if (!granted) {
-        throw new Error('Location permission not granted');
-      }
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        callback
-      );
-      setSubscriber(sub);
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
   useEffect(() => {
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestPermissionsAsync();
+        if (!granted) {
+          throw new Error('Location permission not granted');
+        }
+        const sub = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000,
+            distanceInterval: 10,
+          },
+          callback
+        );
+        setSubscriber(sub);
+      } catch (e) {
+        setErr(e);
+      }
+    };
+
     if(shouldTrack) {
       startWatching();
     } else {
@@ -38,7 +38,7 @@ export default (shouldTrack, callback) => {
         subscriber.remove();
       }
     }
-    
+
   }, [shouldTrack, callback]);
 
   return [err];
