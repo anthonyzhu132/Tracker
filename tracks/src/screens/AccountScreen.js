9c70d171de221fa8eb08797c6d87c5830c2a934d
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-21 19:31:44 -0800

    added finishing styling to each screen navigation

--- tracks/src/screens/AccountScreen.js
+++ tracks/src/screens/AccountScreen.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
@@ -14,6 +15,11 @@ const AccountScreen = () => {
       <Button title="Sign out" onPress={signout} />
     </Spacer>
   </SafeAreaView>
+};
+
+AccountScreen.navigationOptions = {
+  title: 'Account',
+  tabBarIcon: <FontAwesome name="gear" size={20} />
 };
 
 const styles = StyleSheet.create({});
