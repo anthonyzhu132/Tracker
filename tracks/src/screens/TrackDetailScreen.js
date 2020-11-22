7fba99ff12c41bc8d9459271c57892007c844e85
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-21 19:01:46 -0800

    added functionality for TrackDetailScreen to show track name as header

--- tracks/src/screens/TrackDetailScreen.js
+++ tracks/src/screens/TrackDetailScreen.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { Context as TrackContext } from '../context/TrackContext';
 
-const TrackDetailScreen = () => {
+const TrackDetailScreen = ({ navigation }) => {
+  const { state } = useContext(TrackContext);
+  const _id = navigation.getParam('_id');
+  const track = state.find(t => t._id === _id);
+
   return (
-  <Text style={{ fontSize: 48 }}>TrackDetailScreen</Text>
+  <Text style={{ fontSize: 48 }}>{track.name}</Text>
   )
 };
 
--- tracks/src/screens/TrackListScreen.js
+++ tracks/src/screens/TrackListScreen.js
@@ -16,7 +16,10 @@ const TrackListScreen = ({ navigation }) => {
         keyExtractor={item => item._id}
         renderItem={({ item }) => {
           return (
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => 
+              navigation.navigate('TrackDetail', {_id: item._id})
+            }
+            >
               <ListItem chevron title={item.name} />
             </TouchableOpacity>
           );
