74d556e6c794f0e26eb743476ac281bcf0c4d238
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-21 17:36:49 -0800

    formatted with prettier to make things easier to read

--- tracks/src/components/TrackForm.js
+++ tracks/src/components/TrackForm.js
@@ -1,26 +1,39 @@
-import React, { useContext }from 'react';
+import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
 
-
 const TrackForm = () => {
-  const { state: { name, recording }, 
-    startRecording, 
-    stopRecording, 
-    changeName 
+  const {
+    state: { name, recording, locations },
+    startRecording,
+    stopRecording,
+    changeName
   } = useContext(LocationContext);
 
-
-  return <>
-    <Spacer>
-      <Input value={name} onChangeText={changeName} placeholder="Enter Name"/>
-    </Spacer>
-    {recording
-      ? <Button onPress={stopRecording} title="Stop" /> 
-      : <Button onPress={startRecording} title="Start Recording" />
-    }
-  </> 
+  return (
+    <>
+      <Spacer>
+        <Input
+          value={name}
+          onChangeText={changeName}
+          placeholder="Enter name"
+        />
+      </Spacer>
+      <Spacer>
+        {recording ? (
+          <Button title="Stop" onPress={stopRecording} />
+        ) : (
+          <Button title="Start Recording" onPress={startRecording} />
+        )}
+      </Spacer>
+      <Spacer>
+        {!recording && locations.length ? (
+          <Button title="Save Recording" />
+        ) : null}
+      </Spacer>
+    </>
+  );
 };
 
 export default TrackForm;
--- tracks/src/hooks/useLocation.js
+++ tracks/src/hooks/useLocation.js
@@ -1,19 +1,22 @@
 import { useState, useEffect } from 'react';
-import { Accuracy, requestPermissionsAsync, watchPositionAsync } from 'expo-location';
+import {
+  Accuracy,
+  requestPermissionsAsync,
+  watchPositionAsync,
+} from 'expo-location';
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
 
   useEffect(() => {
     let subscriber;
-
     const startWatching = async () => {
       try {
         const { granted } = await requestPermissionsAsync();
         if (!granted) {
           throw new Error('Location permission not granted');
         }
-        subscriber= await watchPositionAsync(
+        subscriber = await watchPositionAsync(
           {
             accuracy: Accuracy.BestForNavigation,
             timeInterval: 1000,
@@ -26,21 +29,20 @@ export default (shouldTrack, callback) => {
       }
     };
 
-    if(shouldTrack) {
+    if (shouldTrack) {
       startWatching();
     } else {
-      if(subscriber) {
+      if (subscriber) {
         subscriber.remove();
       }
       subscriber = null;
     }
 
     return () => {
-      if(subscriber) {
+      if (subscriber) {
         subscriber.remove();
       }
-    }
-
+    };
   }, [shouldTrack, callback]);
 
   return [err];
