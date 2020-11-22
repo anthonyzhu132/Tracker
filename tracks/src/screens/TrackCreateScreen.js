0f88fee9e086dd926546502fa93279f9ec44a1f7
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-11-21 17:34:16 -0800

    fixed formatting for TrackCreateScreen.js to make it easier to read

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,19 +1,24 @@
 import '../_mockLocation';
 import React, { useContext, useCallback } from 'react';
-import useLocation from '../hooks/useLocation';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 import TrackForm from '../components/TrackForm';
 
 const TrackCreateScreen = ({ isFocused }) => {
-  const { state: {recording}, addLocation } = useContext(LocationContext);
-  const callback = useCallback(location => {
-    addLocation(location, recording)
-  }, [recording])
-
+  const {
+    state: { recording },
+    addLocation
+  } = useContext(LocationContext);
+  const callback = useCallback(
+    location => {
+      addLocation(location, recording);
+    },
+    [recording]
+  );
   const [err] = useLocation(isFocused || recording, callback);
 
   return (
